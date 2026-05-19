605b86870dcec2c523429dd2db117d5cf88a80bb
Munendra <naga-munendra.padigala@unilever.com>
2026-05-19 20:56:38 +0530

    fixed cors issue

--- app/main.py
+++ app/main.py
@@ -3,10 +3,20 @@ from app.core.config.settings import settings
 from app.core.exceptions.handlers import register_exception_handlers
 from app.modules.settings.router import router as settings_router
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Accept all origins
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.mount(
